1fd5a6100d1cfee2911e86ff509c4330cbf19859
Tyler Audette <tyleraudette@yahoo.com>
2015-04-07 12:16:32 -0600

    mes

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+// Working code using arrays
+
+
+
 // var victimamt = prompt("How many disaster victims do you wish to enter?")
 // var victimname = []
 // var victimphone =[]
@@ -36,32 +40,104 @@
 // 	"\nVictims: " + victimname + "\nVolunteers: " + volunteername);
 
 
-var victimamt = prompt("How many disaster victims do you wish to enter?");
+
+
+
+// WORKING CODE USING OBJECTS AND TWO LOOPS
+
+// var victimamt = prompt("How many disaster victims do you wish to enter?");
+// var victims = [];
+// var vicNames = [];
+
+
+// 	for(var i=0; i<victimamt; i++){
+// 		var vicName=prompt("Name:");
+// 		var vicPhone= prompt("phone:");
+// 		var vicStreet= prompt("Street:");
+
+// 			var victim = {
+// 				vicName: vicName,
+// 				vicPhone: vicPhone,
+// 				vicStreet : vicStreet
+// 			};
+
+// 				victims.push(victim)
+// 				};
+
+// 			for (var i=0; i<victims.length; i++){
+// 				vicNames.push(victims[i].vicName)
+// 			}			
+
+// var volunteeramt = prompt("How many disaster volunteers do you wish to enter?");
+// var volunteers = [];
+// var volNames = [];
+
+// 	for(var i=0; i<volunteeramt; i++){
+// 		var volName=prompt("Name:");
+// 		var volPhone= prompt("Phone:");
+// 		var volStreet= prompt("Street:");
+
+
+// 			var volunteer = {
+// 				volName: volName,
+// 				volPhone: volPhone,
+// 				volStreet: volStreet
+// 			};
+			
+// 			volunteers.push(volunteer);
+// 			};
+
+// 			for(var i=0; i<volunteers.length; i++){
+// 				volNames.push(volunteers[i].volName);
+// 			}
+
+
+// var summary = alert("Persons in need: " + victimamt + "\nNames: " + vicNames + "\nHelpers: " + 
+// 	volunteeramt + "\nNames: " + volNames);
+
+
+
+// BONUS 1 = assigns value, == checks if is equal
+
+var victimamt = [];
 var victims = [];
 var vicNames = [];
+var askAgain = true
 
 
-	for(var i=0; i<victimamt; i++){
-		var vicName=prompt("Name:");
-		var vicPhone= prompt("phone:");
-		var vicStreet= prompt("Street:");
 
 
-		var victim = {
-			vicName: vicName,
-			vicPhone: vicPhone,
-			vicStreet : vicStreet
-		};
-			victims.push(victim)
+	while (askAgain == true){
+
+		var vicName = prompt("Name:");
+		var vicPhone = prompt("Phone:");
+		var vicStreet = prompt("Street:");
+
+		var con = confirm("Enter more names?");
+		
+		if (con == true){
+			askAgain = true;
+		}
+		else{
+			askAgain = false;
+		}
+
+	}
+
+			var victim = {
+				vicName: vicName,
+				vicPhone: vicPhone,
+				vicStreet : vicStreet
 			};
 
-		for (var i=0; i<victims.length; i++){
-			vicNames.push(victims[i].vicName)
+				victims.push(victim)
+				;
 
-		}
-	
+			for (var i=0; i<victims.length; i++){
+				vicNames.push(victims[i].vicName)
+			}			
 
-			
+// END WORKING WHILE LOOP
 
 var volunteeramt = prompt("How many disaster volunteers do you wish to enter?");
 var volunteers = [];
@@ -79,14 +155,40 @@ var volNames = [];
 				volStreet: volStreet
 			};
 			
-				volunteers.push(volunteer);
-				};
+			volunteers.push(volunteer);
+			};
 
-				for(var i=0; i<volunteers.length; i++){
-					volNames.push(volunteers[i].volName);
-				}
+			for(var i=0; i<volunteers.length; i++){
+				volNames.push(volunteers[i].volName);
+			}
 
 
-var summary = alert("Persons in need: " + victimamt + "\nNames: " + vicNames + "\nHelpers: " + 
-	volunteeramt + "\nNames: " + volNames);
+var vicName = prompt("Enter the Name of someone in need");
+		var st = alert (vicName);
+
+
+// var summary = alert("Persons in need: " + victimamt + "\nNames: " + vicNames + "\nHelpers: " + 
+// 	volunteeramt + "\nNames: " + volNames);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
